459b5369d1f41e0372564adfcb626d68bb044a05
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2021-06-29 18:53:46 -0300

    Aula 3
    Desenvolvimento das seções de Depoimentos, e contato.
    E inserção dos efeitos de rolagem

--- main.js
+++ main.js
@@ -32,3 +32,33 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/* Testimonials Carousel slide swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal: Mostrar elementos qunado rolar a pagina */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home .image, #home .text,
+  #about .image, #about .text,
+  #services .header, #services .card,
+  #testmonials .text, #testmonials .testmonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
